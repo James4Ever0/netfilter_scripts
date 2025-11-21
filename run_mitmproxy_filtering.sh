01129bb831c4dab42e37a155a5c2f70ed00b4ca6
James Brown <randomvoidmail@foxmail.com>
2025-11-21 15:16:32 +0800

    update cli args

--- run_mitmproxy_filtering.sh
+++ run_mitmproxy_filtering.sh
@@ -1,1 +1,2 @@
-mitmproxy -p 1080 --mode socks5 --scripts filter_socks5.py
+# https://serverfault.com/questions/1108971/mitmproxy-as-a-chain-proxy-without-ssl-decryption
+mitmproxy -p 1080 --mode socks5 --scripts filter_socks5.py --allow-hosts ''
